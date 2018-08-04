161428fec590ac1006b2486c0555aa9a9b9fdb50
David Proskin <proskd@gmail.com>
2018-08-04 09:25:03 -0400

    Changed the project to have a basic CollectionView rather than the default master/detail view controller flow.  shows the local IDs in the collection views for now.

---
+++ ImageUploader/Controller/PhotosCollectionViewController.swift
@@ -0,0 +1,57 @@
+//
+//  PhotosCollectionViewController.swift
+//  ImageUploader
+//
+//  Created by David Proskin on 8/4/18.
+//  Copyright © 2018 Dave's Stuff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Photos
+
+
+class PhotosCollectionViewController: UICollectionViewController, PhotosDataProviderDelegate {
+    
+    var photosDataProvider = PhotosDataProvider()
+    
+    override func viewDidLoad() {
+        
+        self.photosDataProvider.fetchPhotos()
+        
+    }
+    
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photosDataProvider.countOfPhotos()
+    }
+    
+    override  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell:PhotoCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCollectionViewCell
+
+        guard let asset = self.photosDataProvider.photoAt(indexPath.row) else {
+            return cell;
+        }
+        configureCell(cell, withAsset: asset)
+        return cell;
+    }
+    
+    func configureCell(_ cell:PhotoCollectionViewCell,  withAsset asset: PHAsset) {
+        cell.label!.text = asset.localIdentifier
+    }
+    
+    // MARK: - Photos Delegate
+    
+    func authStatusChanged(status: PHAuthorizationStatus) {
+        self.photosDataProvider.fetchPhotos()
+        DispatchQueue.main.async {
+            self.collectionView?.reloadData()
+        }
+    }
+    
+}
---
+++ ImageUploader/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  ImageUploader
+//
+//  Created by David Proskin on 8/4/18.
+//  Copyright © 2018 Dave's Stuff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
