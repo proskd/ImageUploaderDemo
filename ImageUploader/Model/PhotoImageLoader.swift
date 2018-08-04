703ae20f2110c3a7cfdf02633ef88f95c71b401e
David Proskin <proskd@gmail.com>
2018-08-04 14:22:11 -0400

    basic implementation of the photo image loader class for thumbnails

--- ImageUploader/Model/PhotoImageLoader.swift
+++ ImageUploader/Model/PhotoImageLoader.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Photos
+import UIKit
+
+class PhotoImageLoader {
+    
+    private static var defaultImageLoader:PhotoImageLoader?;
+    
+    //enforce singleton for this class
+    open class func `default`() -> PhotoImageLoader {
+        if defaultImageLoader == nil {
+            defaultImageLoader = PhotoImageLoader()
+        }
+        
+        return defaultImageLoader!;
+    }
+    
+    private init() {
+    }
+    
+    private let defaultThumbnailSize = CGSize(width: 200, height: 200);
+    
+    public func cancelImageRequest(for identifier:PHImageRequestID) {
+        PHImageManager.default().cancelImageRequest(identifier);
+    }
+    
+    public func loadThumbnail(for asset:PHAsset, resultHandler: @escaping (UIImage?, [AnyHashable : Any]?) -> Swift.Void) -> PHImageRequestID
+    {
+    
+        let options:PHImageRequestOptions = PHImageRequestOptions()
+        options.version = .current
+        options.deliveryMode = .fastFormat
+        options.resizeMode = .fast
+        options.isNetworkAccessAllowed = true
+    
+        return PHImageManager.default().requestImage(for: asset, targetSize: defaultThumbnailSize, contentMode: .aspectFill, options: options, resultHandler: resultHandler);
+    }
+}
