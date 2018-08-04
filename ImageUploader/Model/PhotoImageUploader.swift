195df06e95331e055c07c56d48458521c785309a
David Proskin <proskd@gmail.com>
2018-08-04 15:55:31 -0400

    Added a class for uploading images

---
+++ ImageUploader/Model/PhotoImageUploader.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoImageUploader.swift
+//  ImageUploader
+//
+//  Created by David Proskin on 8/4/18.
+//  Copyright © 2018 Dave's Stuff. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+
+class PhotoImageUploader {
+    
+}
